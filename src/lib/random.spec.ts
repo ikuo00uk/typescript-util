a512be05a3883fb3caca1a8df91a0d7ad11f0b27
Ikuo Hirakawa <ikuo@frasco-inc.co.jp>
2019-10-16 15:55:46 +0900

    feat(random): add new func

--- src/lib/random.spec.ts
+++ src/lib/random.spec.ts
@@ -1,10 +1,20 @@
 // tslint:disable:no-expression-statement
 import test from 'ava';
-import { generateRandomUserId } from './random';
+import { getRandomInt, generateRandomUserId, regenerateArray, shuffle } from './random';
 
-test(
-  'generateRandomUserId', t => {
-    const uid =generateRandomUserId(10);
-    console.log(uid);
-    t.true(typeof uid == 'string');
+test('generateRandomUserId', t => {
+  const uid = generateRandomUserId(10);
+  t.true(typeof uid === 'string');
 });
+
+test('getRandomInt', t=> {
+  t.true(typeof getRandomInt(0,100) === 'number')
+})
+
+test('regenerateArray', t=> {
+  t.true(typeof regenerateArray([1,2,3,4,5],2) === 'object')
+})
+
+test('shuffle', t=> {
+  t.true(typeof shuffle([1,2,3,4,5]) === 'object')
+})
--- src/lib/random.ts
+++ src/lib/random.ts
@@ -1,4 +1,52 @@
 /**
+ * ランダムに数字を取得する
+ *
+ * ### Example
+ * ```js
+ * import { getRandomInt } from './random'
+ * console.log(getRandomInt(0,100))
+ * // => 0~100までの間でランダムな数字
+ * ```
+ *
+ * @param min: 最小値
+ * @param max: 最大値
+ * @return ランダム数字
+ *
+ */
+const getRandomInt =(min:number, max:number): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+/**
+ * 配列を一定の長さで再生成する
+ * 
+ * ### Example
+ * ```js
+ * import { regenerateArray } from './random'
+ * console.log(regenerateArray([0,1,2,3,4,5],2)))
+ * // => 2インデックスで、新しい配列が生成される。
+ * ```
+ *
+ * @param array: 配列
+ * @param num: インデックス
+ * @return 再生成した配列
+ */
+const regenerateArray = (array:Array<any>, num:number): Array<any> => {
+  let a = array
+  let t:Array<any> = [], r = [];
+  let l = a.length
+  let n = num < l ? num : l
+  while (n-- > 0) {
+    let i = (Math.random() * l) | 0
+    r[n] = t[i] || a[i]
+    --l
+    t[i] = t[l] || a[l]
+  }
+  return r
+}
+
+
+/**
  * 無作為にユーザーIDを生成する(SNSなどの)
  *
  * ### Example (es module)
@@ -8,15 +56,38 @@
  * // => 8桁のランダムユーザー名
  * ```
  *
- **/
+ * @param l: 生成したい文字列
+ * @returns ランダム文字列
+ *
+ */
 const generateRandomUserId = (l: number = 8): string => {
-  const c = 'abcdefghijklmnopqrstuvwxyz0123456789_'
-  const cl = c.length
+  const c = 'abcdefghijklmnopqrstuvwxyz0123456789_';
+  const cl = c.length;
   let _id = '';
   for (let i = 0; i < l; i++) {
-    _id += c[Math.floor(Math.random() * cl)]
+    _id += c[Math.floor(Math.random() * cl)];
   }
-  return _id
+  return _id;
+};
+
+/**
+ * 配列をシャッフルして返す
+ *
+ * @param array シャッフルしたい配列
+ * @return array
+ */
+const shuffle = (array:Array<any>) => {
+  let i = array.length
+  while (i) {
+    const j = Math.floor(Math.random() * i)
+    let t = array[--i]
+    array[i] = array[j]
+    array[j] = t
+  }
+  return array
 }
 
-export { generateRandomUserId }
+
+
+
+export { generateRandomUserId, getRandomInt, regenerateArray, shuffle };
